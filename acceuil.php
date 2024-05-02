d8fa9ed1ca4e7323fd0c817bde9b8092b5a8eca7
Lorichag <133487399+Lorichag@users.noreply.github.com>
2024-05-02 09:50:46 +0200

    Add files via upload

---
+++ acceuil.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="acceuil.css">
+</head>
+<body>
+
+<p id="connexion">Connexion</p>
+<p id="titre1" >Pet Crush</p>
+<p id="titre2">Le site de rencontre pour animaux</p>
+<p id="slogan">Car les animaux aussi ont le droit à l'amour</p>
+
+
+</body>
+</html>
