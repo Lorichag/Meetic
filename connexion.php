e29325bb16ced2ed5a7475424bc23debdf8873ca
Lorichag <133487399+Lorichag@users.noreply.github.com>
2024-05-03 10:27:19 +0200

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="acceuil.css">
+</head>
+<body>
+
+<p id="titre1" >Pet Crush</p>
+<form style="text-align: center;" method="post" action="verrification.php">
+Utlisateur:
+<input type="text" name="Nom">
+<BR>
+<BR>
+Mot de passe:
+<input type="password" name="Ps">
+<BR>
+<BR>
+<input type="submit" value="connexion" >
+<BR>
+<BR>
+<BR>
+<BR>
+<a id="Inscription" href="inscription.php">s'inscrire</a>
+</form>
+
+
+</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="acceuil.css">
+</head>
+<body>
+<p id="titre1" >Pet Crush</p>
+<form style="text-align: center;" method="post" action="verrification.php">
+Utlisateur:
+<input type="text" name="Nom">
+<BR>
+<BR>
+Mot de passe:
+<input type="password" name="Ps">
+<BR>
+<BR>
+Confirmer mot de passe:
+<input type="password" name="Ps">
+<BR>
+<BR>
+<input type="submit" value="valider" >
+</form>
+
+
+</body>
+</html>
