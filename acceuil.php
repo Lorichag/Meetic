ddc899797094336bad2c91ab64b05e0c2f138056
Lorichag <133487399+Lorichag@users.noreply.github.com>
2024-05-03 10:26:34 +0200

    Update acceuil.php

--- acceuil.php
+++ acceuil.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 
-<p id="connexion">Connexion</p>
+<a id="connexion" href="connexion.php">Connexion</a>
 <p id="titre1" >Pet Crush</p>
 <p id="titre2">Le site de rencontre pour animaux</p>
 <p id="slogan">Car les animaux aussi ont le droit à l'amour</p>
