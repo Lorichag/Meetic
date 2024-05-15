5c33e72c4d79e74a5a6f2477f8a879af16f8a891
Cupo2fleurs <153006714+Cupo2fleurs@users.noreply.github.com>
2024-05-15 19:50:10 +0200

    Add files via upload

---
+++ message.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+<div id="bande">
+ 
+ 
+<p id="connarddelogodemerdemaldimensionné"><img src="image/logo.png"></p> 
+<div><img id="p1" src="image/home.png" ></div>
+<div><img id="p2" src="image/loupe.png"></div>
+
+
+<a href="message.php"><img id="p3" src="image/message.png"></a>
+
+
+
+<a href="profil.php"><img id="p4" src="image/profil.png"></a>
+ 
+<div><img id="p5" src="image/parametre.png"></div>
+</div>
+
+<div class="profil">
+<table>
+
+
+</table>
+
+
+</div>
+
+
+<style type="text/css">
+
+
+#bande {
+    position: absolute;
+    float: left;
+    width: 5%;
+    height: 100vh;
+    background-color: gray;
+    margin-right: 1%;
+}
+ 
+#bande div {
+    margin-top: 10px;
+}
+ 
+#bande img {
+    width: 50%;
+    height:5%;
+    padding-left:25%;
+    padding-top:30%;
+    display: block;
+}
+ 
+
+#p5 {
+ margin-top:400%;
+}
+
+.profil {
+position: absolute;
+    width: 30%;
+    border: solid black 2px;
+    height: 100vh;
+    margin-left: 5%;
+}
+
+</style>
+
+
+
+
+
+
+</head>
+</html>
