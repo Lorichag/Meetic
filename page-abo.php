c63320ef3cab34c0589656d2896c03bf0314ad07
Cupo2fleurs <153006714+Cupo2fleurs@users.noreply.github.com>
2024-05-27 14:27:57 +0200

    Add files via upload

--- page-abo.php
+++ page-abo.php
@@ -1,3 +1,10 @@
+
+
+<?php
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
@@ -9,7 +16,7 @@
 <h2>Offre Foufou</h2>
 <h1> 5.99€ </h1>
 <h1>Pour 1 mois </h1>
-<a href="paiement.php">Start Now</a>
+<a href="paiement.php?offre=foufou">Start Now</a>
 <BR>
 <br>
 <p>Acces au site à son plein potentiel pour rencontrer l'amseur de ton animal</p>
@@ -21,7 +28,7 @@
 <h2>Offre Sauvage</h2>
 <h1>15.99€</h1>
 <h1>Pour 3 mois !</h1>
-<a href="paiement.php">Start Now</a>
+<a href="paiement.php?offre=sauvage">Start Now</a>
 <BR>
 <br>
 <p>Acces au site à son plein potentiel pour rencontrer l'amseur de ton animal</p>
@@ -33,21 +40,21 @@
 <h2>Offre Roi</h2>
 <h1>59.99€</h1>
 <h1>Pour 1 ANS !!!</h1>
-<a href="paiement.php">Start Now</a>
+<a href="paiement.php?offre=roi">Start Now</a>
 <BR>
 <br>
 <p>Acces au site à son plein potentiel pour rencontrer l'amseur de ton animal</p>
 
 </div>
+</body>
+
+
 
 <style type="text/css">
 
 body {
-background-image: url('https://www.fond-ecran-hd.net/Public/uploads/2019-01-13/thumbs-1/1321.jpg');
+background:radial-gradient(circle,rgba(34,193,195,1) 20%, rgba(253,187,45,1)90%);
 height:97vh;
-background-position: center;
-background-size: cover;
-
 }
 
 .Foufou{
@@ -60,7 +67,8 @@ background-size: cover;
  background-size: cover;
  margin-top: 175px ;
  margin-right: 10px ;
- margin-left:350px;
+ margin-left:400px;
+ border-radius:10px;
 }
 
 .Foufou h2 {
@@ -100,7 +108,8 @@ float:left;
   background-size: cover;
  margin-top: 7% ;
  margin-right: 1% ;
- margin-left:750px;
+ margin-left:800px;
+  border-radius:10px;
 }
 
 .Sauvage h1 {
@@ -135,7 +144,8 @@ float:left;
   background-size: cover;
  margin-top: 8% ;
  margin-right: 1% ;
- margin-left:1150px;
+ margin-left:1200px;
+  border-radius:10px;
 }
 
 .Roi h1 {
@@ -163,5 +173,5 @@ color:white;
 </style>
 
 
-</body>
+
 </html>
--- paiement.php
+++ paiement.php
@@ -1,3 +1,26 @@
+
+
+<?php
+if (isset($_GET["offre"])) {
+    echo "Offre: " . htmlspecialchars($_GET["offre"]) . "<br>";
+} else {
+    echo "Le paramètre 'offre' n'est pas défini dans l'URL.<br>";
+}
+$bdd = new PDO('mysql:host=prc-students-mysql.cy-tech.fr;port=3306;dbname=rencontres;charset=utf8', 'guesdonaxe', 'pho2eacoo0Vei2e');
+session_start();
+if(isset($_POST["fric"])){
+$abo = 1;
+$id = $_SESSION["id"];
+$fin_abo = 2004;
+$updateUser = $bdd->prepare('UPDATE users SET abo = ?, fin_abo = ? WHERE id = ?');
+$updateUser->execute(array($abo, $fin_abo, $id));
+
+
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
@@ -6,10 +29,14 @@
 <div class="paye">
 <h1>Paiement</h1>
 <p>Numéro de carte <span id="p1">Expiration</span><span id="p2">CVC</span></p>
+<form method="POST" action="">
 <input class="carte1" type="text" name="num-carte"  autocomplete="off"  placeholder="1234 1234 1234 1234" required>
 <input class="carte2" type="text" name="exp-carte"  autocomplete="off"  placeholder="MM/YY" required>
 <input class="carte3" type="text" name="CVC"  autocomplete="off"  placeholder="CVC" required>
+<input type="submit" value="Valider" name="fric">
+</form>
 </div>
+
 
 <style type="text/css">
 #p1 {
@@ -32,6 +59,11 @@ width:100px;
 }
 
 </style>
+
+
+
+
+
 
 
 </body>
