7d963e41f6893552a2c43d4a1d254688808aba42
Cupo2fleurs <153006714+Cupo2fleurs@users.noreply.github.com>
2024-06-07 12:28:25 +0200

    Add files via upload

--- Paramètres.php
+++ Paramètres.php
@@ -1,20 +1,18 @@
 <?php 
-
 session_start();
 if(!isset($_SESSION['pseudo'])){
     header('Location: login.php');
     exit();
 }
 
-$bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8;', 'root');
+$bdd = new PDO('mysql:host=prc-students-mysql.cy-tech.fr;port=3306;dbname=rencontres;charset=utf8', 'guesdonaxe', 'pho2eacoo0Vei2e');
 
 $recupinfo = $bdd->prepare('SELECT * FROM users WHERE pseudo= ?');
 $recupinfo->execute(array($_SESSION['pseudo']));
 $info = $recupinfo->fetch();
-$pseudo=$_SESSION['pseudo'];
+$pseudo = $_SESSION['pseudo'];
 $nom_dossier = "uploads";
 
-// Créer un dossier s'il n'existe pas déjà
 if (!is_dir($nom_dossier)) {
     mkdir($nom_dossier);
 }
@@ -22,40 +20,90 @@ if (!is_dir($nom_dossier)) {
 if(isset($_POST['valider'])){
     $espece = $_POST['espece'];
     $race = $_POST['race'];
-    $profil=$_POST['profil'];	
-   
-   if (isset($_FILES['profil']) && $_FILES['profil']['error'] == 0) {
+    $bio = $_POST['bio'];
+
+    // Définir le nom du fichier de profil initialement à l'ancien nom
+    $newProfilName = $info['profil'];
+
+    if (isset($_FILES['profil']) && $_FILES['profil']['error'] == 0) {
         $profilFileType = strtolower(pathinfo($_FILES["profil"]["name"], PATHINFO_EXTENSION));
-        // Générer un nouveau nom de fichier unique
-        $target1 = 'uploads/';
+        $target1 = 'image/';
         $newProfilName = $_SESSION['pseudo'].'_profil.'.$profilFileType;
         $target_profil = $target1.$newProfilName;
-        move_uploaded_file($_FILES["profil"]["tmp_name"], $target_profil);	
+        move_uploaded_file($_FILES["profil"]["tmp_name"], $target_profil);    
     }
+    
     $verife = $bdd->prepare('SELECT * FROM image WHERE pseudo = ?');
     $verife->execute(array($pseudo));
-    echo $verife->rowCount();
+    
     if($verife->rowCount() < 5){
-	    if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
-		$imageFileType = strtolower(pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION));
-		// Générer un nouveau nom de fichier unique
-		$target = 'uploads/';
-		$newFileName = uniqid('image_', true) . '.'.$imageFileType;
-		$target_file = $target.$newFileName;	
-		if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)){
-	    		$insertImage = $bdd->prepare('INSERT INTO image(pseudo,nom_image) VALUES(?, ?)');
-	    		$insertImage->execute(array($_SESSION['pseudo'],$newFileName));
-	    	}
-	    	else {
-		    echo "Désolé, une erreur est survenue lors du téléchargement de votre fichier.";
-		}
-	    }
+        if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+            $imageFileType = strtolower(pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION));
+            $target = 'uploads/';
+            $newFileName = uniqid('image_', true) . '.'.$imageFileType;
+            $target_file = $target.$newFileName;    
+            if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)){
+                $insertImage = $bdd->prepare('INSERT INTO image(pseudo, nom_image) VALUES(?, ?)');
+                $insertImage->execute(array($_SESSION['pseudo'], $newFileName));
+            } else {
+                echo "Désolé, une erreur est survenue lors du téléchargement de votre fichier.";
+            }
+        }
     }
+    
+    $updateUser = $bdd->prepare('UPDATE users SET espece = ?, race = ?, profil = ?, bio = ? WHERE pseudo = ?');
+    $updateUser->execute(array($espece, $race, $newProfilName, $bio, $_SESSION['pseudo']));
 
-	
-    $updateUser = $bdd->prepare('UPDATE users SET espece = ? , race = ? , profil = ? WHERE pseudo = ?');
-    $updateUser->execute(array($espece,$race,$newProfilName,$_SESSION['pseudo']));
+    // Reload the page
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
 }
+
+if (isset($_POST['unblock_user'])) {
+    $userIdToUnblock = $_POST['user_id'];
+    $deleteBlock = $bdd->prepare('DELETE FROM bloquer WHERE id_au = ? AND id_sig = ?');
+    $deleteBlock->execute(array($info['id'], $userIdToUnblock));
+
+    // Reload the page
+    header('Location: ' . $_SERVER['PHP_SELF']);
+    exit();
+}
+
+// Fetch blocked users
+$blockedUsers = $bdd->prepare('SELECT users.id, users.pseudo, users.age, users.sexe, users.espece, users.race 
+                               FROM bloquer 
+                               JOIN users ON bloquer.id_sig = users.id 
+                               WHERE bloquer.id_au = ?');
+$blockedUsers->execute(array($info['id']));
+
+
+$recupUser = $bdd->prepare('SELECT * FROM users WHERE id NOT IN 
+    (SELECT id_sig FROM bloquer WHERE id_au = ?) 
+    AND id NOT IN (SELECT id_au FROM bloquer WHERE id_sig = ?) 
+    AND id != ? ORDER BY id DESC');
+$recupUser->execute(array($_SESSION['id'], $_SESSION['id'], $_SESSION['id']));
+
+
+if (isset($_GET['q']) && !empty($_GET['q'])) {
+    $q = htmlspecialchars($_GET['q']);
+    
+    $recupUser = $bdd->prepare('SELECT id, pseudo, profil FROM users 
+                                WHERE id NOT IN (SELECT id_sig FROM bloquer WHERE id_au = ?) 
+                                AND id NOT IN (SELECT id_au FROM bloquer WHERE id_sig = ?) 
+                                AND pseudo LIKE ? 
+                                ORDER BY id DESC');
+    $recupUser->execute(array($_SESSION['id'], $_SESSION['id'], "%$q%"));
+} else {
+    $recupUser = $bdd->prepare('SELECT id, pseudo, profil FROM users 
+                                WHERE id NOT IN (SELECT id_sig FROM bloquer WHERE id_au = ?) 
+                                AND id NOT IN (SELECT id_au FROM bloquer WHERE id_sig = ?) 
+                                ORDER BY id DESC');
+    $recupUser->execute(array($_SESSION['id'], $_SESSION['id']));
+}
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -73,7 +121,7 @@ if(isset($_POST['valider'])){
 <body>
 <div class="sidebar">
     <div class="logo-details">
-        <img src="logo.png" class="icon">
+        <img src="image/logo.png" class="icon">
         <a href="connecting.php?id=<?= $_SESSION['id']; ?>">
             <div class="logo_name">AniMate</div>
         </a>
@@ -81,8 +129,10 @@ if(isset($_POST['valider'])){
     </div>
     <ul class="nav-list">
         <li>
-            <i class='bx bx-search'></i>
-            <input type="text" placeholder="Search...">
+        <i class='bx bx-search' ></i>
+            <form method="GET" action="connecting.php?id=<?= $_SESSION['id']; ?>">
+                <input type="search" name="q" placeholder="Recherche..." />
+            </form>
         </li>
         <li>
             <a href="connecting.php?id=<?= $_SESSION['id']; ?>">
@@ -109,14 +159,14 @@ if(isset($_POST['valider'])){
             </a>
         </li>
         <li>
-            <a href="Paramètres.php">
+            <a href="Paramètres.php?id=<?= $_SESSION['id']; ?>">
                 <i class='bx bx-cog'></i>
                 <span class="links_name">Paramètres</span>
             </a>
         </li>
         <li class="profile">
             <div class="profile-details">
-                <img src="profile.jpg">
+                <img src="image/<?= $info['profil']; ?>">
                 <div class="name_job">
                     <div class="name"><?= $_SESSION['pseudo']; ?></div>
                 </div>
@@ -140,10 +190,17 @@ if(isset($_POST['valider'])){
     ?>
         <form align="center" class="te" >
         <h3>Informations Personnelles</h3>
-        <div class="single">
+        <div class="profile-image">
+            <img src="image/<?= $info['profil']; ?>" alt="Profil Image" style="display:flex; justify-content:center;" width="150">
+        </div>
+        <div class="double">
             <div class="Case"> 
                 <i class='bx bx-user-circle' id="test"></i>
                 <input class="Custom" type="text" disabled="disabled" name="pseudo" value="<?= $info['pseudo']; ?>">
+            </div>
+            <div class="Case"> 
+                <img src="image/courrier.png" id="test">
+                <input class="Custom" type="text" disabled="disabled" name="mail" value="<?= $info['pseudo']; ?>">
             </div>
         </div>
 
@@ -153,64 +210,99 @@ if(isset($_POST['valider'])){
                 <input class="Custom" type="text" disabled="disabled" name="age" value="<?= $info['age']; ?> ans">
             </div>
             <div class="Case">
-                <img src="sexe.png" id="test">
+                <img src="image/sexe.png" id="test">
                 <input class="Custom" type="text" disabled="disabled" name="sexe" value="<?= $info['sexe']; ?>">
             </div>
         </div>
         <div class="double">
             <div class="Case"> 
-                <img src="dog.png"  id="test">
-                <input class="Custom" type="text" disabled="disabled" name="age" value="<?= $info['espece']; ?>">
+                <img src="image/dog.png"  id="test">
+                <input class="Custom" type="text" disabled="disabled" name="espece" value="<?= $info['espece']; ?>">
             </div>
             <div class="Case"> 
-                <img src="race.png"  id="test">
-                <input class="Custom" type="text" disabled="disabled" name="sexe" value="<?= $info['race']; ?>">
+                <img src="image/race.png"  id="test">
+                <input class="Custom" type="text" disabled="disabled" name="race" value="<?= $info['race']; ?>">
             </div>
         </div>
-        <p>Photo de profil: <?= $info['profil']; ?></p>
+        <div class="single">
+            <p>Bio: <textarea name="bio"><?= $info['bio']; ?></textarea></p>
+        </div>
+        </br>
+        <h3>Mes photos:</h3>
+        <?php
+            $recupImages = $bdd->prepare('SELECT nom_image FROM image WHERE pseudo = ?');
+            $recupImages->execute(array($pseudo));
+            while ($image = $recupImages->fetch()) {
+                echo '<img src="uploads/'.$image['nom_image'].'" alt="" width="150">';
+            }
+        ?>
         
         <a href="?modifier=1"></a>
         <div class="button-container">
         <input class="button" value="Modifier" onclick="window.location.href = '?modifier=1';">
         </div>
-    
         </form>
+
+        <h3>Personnes bloquées:</h3>
+        <ul>
+        <?php
+            while ($blocked = $blockedUsers->fetch()) {
+                echo '<li>Pseudo: ' . $blocked['pseudo'] . ', Age: ' . $blocked['age'] . ', Sexe: ' . $blocked['sexe'] . ', Espece: ' . $blocked['espece'] . ', Race: ' . $blocked['race'];
+                echo '<form method="POST" action="">
+                        <input type="hidden" name="user_id" value="' . $blocked['id'] . '">
+                        <input type="submit" name="unblock_user" value="Débloquer">
+                      </form>
+                      </li>';
+            }
+        ?>
+        </ul>
+
     <?php 
     } 
     else { 
     ?>
         <form method="POST"  align="center" class="te" action="Paramètres.php" enctype="multipart/form-data">
         <h3>Informations Personnelles</h3>
-        <div class="single">
+        <div class="profile-image">
+            <img src="image/<?= $info['profil']; ?>" alt="Profil Image" style="display:flex; justify-content:center;" width="150">
+        </div>
+        <div class="double">
             <div class="Case"> 
                 <i class='bx bx-user-circle' id="test"></i>
                 <input class="Custom" type="text" disabled="disabled" name="pseudo" value="<?= $info['pseudo']; ?>">
             </div>
+            <div class="Case"> 
+                <img src="image/courrier.png" id="test">
+                <input class="Custom" type="text" disabled="disabled" name="mail" value="<?= $info['mail']; ?>">
+            </div>
         </div>
-
         <div class="double">
             <div class="Case"> 
                 <i class='bx bxs-calendar ' id="test" ></i>
                 <input class="Custom" type="text" disabled="disabled" name="age" value="<?= $info['age']; ?>">
             </div>
             <div class="Case">
-                <img src="sexe.png" id="test">
+                <img src="image/sexe.png" id="test">
                 <input class="Custom" type="text" disabled="disabled" name="sexe" value="<?= $info['sexe']; ?>">
             </div>
         </div>
         <div class="double">
             <div class="Case"> 
-                <img src="dog.png"  id="test">
+                <img src="image/dog.png"  id="test">
                 <input class="Custom" type="text" name="espece" value="<?= $info['espece']; ?>">
             </div>
             <div class="Case"> 
-                <img src="race.png"  id="test">
+                <img src="image/race.png"  id="test">
                 <input class="Custom" type="text"name="race" value="<?= $info['race']; ?>">
             </div>
         </div>
+        <div class="single">
+            <p>Bio: <textarea name="bio"><?= $info['bio']; ?></textarea></p>
+        </div>
+        </br>
 
-            <p>Photo de profil: <input type="file" name="profil" value="<?= $info['profil']; ?>"></p>
-            <p>Ajouter une photo: <input type="file" name="image" id="fileToUpload"></p>
+        <p>Photo de profil: <input type="file" name="profil"></p>
+        <p>Ajouter une photo: <input type="file" name="image" id="fileToUpload"></p>
             <div class="button-container">
                 <input class="button" type="submit" name="valider" value="Enregistrer les modifications">
             </div>
@@ -222,7 +314,7 @@ if(isset($_POST['valider'])){
 </div>
 
 <div class="pub">
-      <img class="t" src="https://nuostore.com/wp-content/uploads/2023/11/IBANEZ-AE295WK-300x775.jpg">
+        <img id="randomImage" class="t" src="" alt="Publicité">
     </div>
 
 </div>
@@ -231,7 +323,16 @@ if(isset($_POST['valider'])){
     </body>
 </html>
 
+<script>
+        const images = ["image/Pub.png", "image/Pub1.png", "image/Pub2.png", "image/Pub3.png", "image/Pub4.png"];
 
+        function getRandomImage() {
+            const randomIndex = Math.floor(Math.random() * images.length);
+            return images[randomIndex];
+        }
+        // Sélectionner l'élément img et définir son attribut src
+        document.getElementById('randomImage').src = getRandomImage();
+    </script>
 <style>
 
 @font-face {
@@ -323,7 +424,6 @@ h3 {
 
 
 
-
 .Custom {
   border: none;
   display: block;
@@ -396,26 +496,32 @@ height:770px;
 
 }
 
+.profile-image {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+
 </style>
 
 <script>
-let sidebar = document.querySelector(".sidebar");
- let decaler = document.querySelector((".decaler"))
-  let closeBtn = document.querySelector("#btn");
-  let searchBtn = document.querySelector(".bx-search");
-  closeBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("open");
-    decaler.classList.toggle("open");
-  });
-  searchBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("open");
-    decaler.classList.toggle("open");
-  });
+    let sidebar = document.querySelector(".sidebar");
+    let decaler = document.querySelector((".decaler"))
+    let closeBtn = document.querySelector("#btn");
+    let searchBtn = document.querySelector(".bx-search");
+    closeBtn.addEventListener("click", ()=>{
+        sidebar.classList.toggle("open");
+        decaler.classList.toggle("open");
+    });
+    searchBtn.addEventListener("click", ()=>{
+        sidebar.classList.toggle("open");
+        decaler.classList.toggle("open");
+    });
 
     function redirectMessagerie(userId) {
-      window.location.href = "profil.php?id=" + userId;
+        window.location.href = "profil.php?id=" + userId;
     }
     document.getElementById('log_out').addEventListener('click', function() {
-      document.getElementById('logout-form').submit();
+        document.getElementById('logout-form').submit();
     });
   </script>
