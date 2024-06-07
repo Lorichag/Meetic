581a92b5a33809c53cbca99ee726a448ec43f8f0
MathisLanceRichardot <153006795+MathisLanceRichardot@users.noreply.github.com>
2024-06-07 11:13:09 +0200

    Add files via upload

--- messagerie.php
+++ messagerie.php
@@ -14,6 +14,7 @@ if(isset($getid) && !empty($getid)){
     if($recupUser->rowCount() > 0){
         $user = $recupUser->fetch();
         $cible = htmlspecialchars($user['pseudo']);
+        $photo = htmlspecialchars($user['profil']);
         if(isset($_POST['message'])) {
             $date = date('Y-m-d H:i:s');
             $message = htmlspecialchars($_POST['message']);
@@ -30,19 +31,22 @@ if(isset($getid) && !empty($getid)){
 }
 
 if(isset($_POST['supprimer'])) {
+error_log("bon");
   $messagesupp = $_POST['supprimer'];
   $supprimerMessage = $bdd->prepare('DELETE FROM messages WHERE id = ?');
   $supprimerMessage->execute(array($messagesupp));
 }
 
 if(isset($_POST['signaler'])) {
+    
     $messageId = $_POST['message'];
     $motif = $_POST['motif'];
     $date = date('Y-m-d H:i:s');
     $utilisateur_signaler = $getid;
     $type=$_POST['type'];
-    $insertDemande = $bdd->prepare('INSERT INTO demandes(utilisateur, date, type, motif, description, utilisateur_signaler) VALUES(?, ?, ?, ?, ?, ?)');
+    $insertDemande = $bdd->prepare('INSERT INTO demandes(utilisateur, dates, types, motif, descriptions, utilisateur_signaler) VALUES(?, ?, ?, ?, ?, ?)');
     $insertDemande->execute(array($_SESSION['id'], $date, $type, $motif, $messageId, $utilisateur_signaler)); 
+
 }
 ?>
 
@@ -59,113 +63,109 @@ if(isset($_POST['signaler'])) {
 <body>
 <div class="sidebar">
     <div class="logo-details">
-        <img src="logo.png" class="icon">
-        <a href="connecting.php?id=<?= $_SESSION['id']; ?>">
-            <div class="logo_name">AniMate</div>
-        </a>
-        <i class='bx bx-menu' id="btn"></i>
+      <img src="image\logo.png" class="icon"></img>
+        <div class="logo_name">AniMate</div>
+        <i class='bx bx-menu' id="btn" ></i>
     </div>
     <ul class="nav-list">
-        <li>
-            <i class='bx bx-search'></i>
-            <input type="text" placeholder="Search...">
-        </li>
-        <li>
-            <a href="connecting.php?id=<?= $_SESSION['id']; ?>">
-                <i class='bx bx-grid-alt'></i>
-                <span class="links_name">Accueil</span>
-            </a>
-        </li>
-        <li>
-            <a href="profil.php?id=<?= $_SESSION['id']; ?>">
-                <i class='bx bx-user'></i>
-                <span class="links_name">Profil</span>
-            </a>
-        </li>
-        <li>
-            <a href="messagerie.php?id=<?= $_SESSION['id']; ?>">
-                <i class='bx bx-chat'></i>
-                <span class="links_name">Messages</span>
-            </a>
-        </li>
-        <li>
-            <a href="page-abo.php?id=<?= $_SESSION['id']; ?>">
-                <i class='bx bxl-paypal'></i>
-                <span class="links_name">Abonnement</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <i class='bx bx-cog'></i>
-                <span class="links_name">Paramètres</span>
-            </a>
-        </li>
-        <li class="profile">
-            <div class="profile-details">
-                <img src="profile.jpg">
-                <div class="name_job">
-                    <div class="name"><?= $_SESSION['pseudo']; ?></div>
-                </div>
-            </div>
-            <form method="POST" action="" id="logout-form" style="display: inline;">
+      <li>
+        
+          <i class='bx bx-search' ></i>
+         <input type="text" placeholder="Search...">
+      </li>
+      <li>
+        <a href="connecting.php?id=<?= $_SESSION['id']; ?>">
+          <i class='bx bx-grid-alt'></i>
+          <span class="links_name">Accueil</span>
+        </a>
+      </li>
+      <li>
+       <a href="profil.php?id=<?= $_SESSION['id']; ?>">
+         <i class='bx bx-user' ></i>
+         <span class="links_name">Profile</span>
+       </a>
+     </li>
+     <li>
+       <a href="messagerie.php?id=<?= $_SESSION['id']; ?>">
+         <i class='bx bx-chat' ></i>
+         <span class="links_name">Messages</span>
+       </a>
+     </li>
+     <li>
+     <li>
+        <a href="page-abo.php?id=<?= $_SESSION['id']; ?>">
+          <i class='bx bxl-paypal'></i>
+          <span class="links_name">Abonnement</span>
+        </a>
+      </li>
+    <li>
+       <a href="Paramètres.php?id=<?= $_SESSION['id']; ?>">
+         <i class='bx bx-cog' ></i>
+         <span class="links_name">Paramètres</span>
+       </a>
+     </li>
+     <li class="profile">
+         <div class="profile-details">
+         <img src="image\<?=$_SESSION['id']['profil']?>" class="icon1">
+           <div class="name_job">
+             <div class="name"><?= $_SESSION['pseudo']; ?></div>
+           </div>
+         </div>
+         <form method="POST" action="" id="logout-form" style="display: inline;">
                 <input type="hidden" name="d" value="logout">
                 <i class='bx bx-log-out' id="log_out" style="cursor: pointer;"></i>
-            </form>
-        </li>
+          </form>
+     </li>
     </ul>
-</div>
+  </div>
 
-<div id="signalement" style="display: none;">
-    <span id='close' class="close">X</span>  
-    <p>Signalement</p>
-        <br>
-        <button class="signalement" data-button="Acte sexuel">Acte sexuel</button>
-        <br>
-        <button class="signalement" data-button="Racisme">Racisme</button>
-        <br>
-        <button class="signalement" data-button="Antisémite">Antisémite</button>
-        <br>
-        <button class="signalement" data-button="Violence textuelle">Violence textuelle</button>
-        <br>
-</div>
-
-<div id="signalementprof" style="display: none;">
-    <span id='close' class="close">X</span>  
-    <p>Signalement</p>
-    <button class="signalement" data-button="Arnaqueur">Arnaqueur</button>
-    <br>
-    <button class="signalement" data-button="Usurpation de compte">Usurpation de compte</button>
-    <br>
-</div>
-
-<div class="Messagerie">
-    <div class="Liste">
-        <?php 
-            $recupUser = $bdd->query('SELECT * FROM users');
-            while($user = $recupUser->fetch()){
-                if($user['id'] != $_SESSION['id']){
-        ?>
-            <button class="bouton" onclick="redirectMessagerie(<?= $user['id']; ?>)">
-                <div class="case">
-                    <img src="omg.png" class="icon1">
-                    <p class="ut"><?= $user['pseudo']; ?></p>
-                </div>
-            </button>
-        <?php
-                }
-            }
-        ?>
+  <div id="signalement" class="modal">
+        <div class="modal-content">
+            <span id="close" class="close" onclick="closeModal()">&times;</span>  
+            <p>Signalement</p>
+            <br>
+            <button class="signalement" data-button="Acte sexuel">Acte sexuel</button>
+            <br>
+            <button class="signalement" data-button="Racisme">Racisme</button>
+            <br>
+            <button class="signalement" data-button="Antisémite">Antisémite</button>
+            <br>
+            <button class="signalement" data-button="Violence textuelle">Violence textuelle</button>
+            <br>
+        </div>
     </div>
 
-    <div class="ChatConteneur">
-        <div class="header">
-            <p onclick="optionProfil()"><?= $cible ?></p>
-            <div class="options-container-profil">
-                <div class="option-profil" style="display: none;">
-                    <button class="bloqueprof-btn" data-pseudo-id="<?= $user['id']; ?>">Bloquer</button>
-                    <button class="reportprof-btn" data-pseudo-id="<?= $user['id']; ?>">Signaler</button>
-                </div>
+    <div class="overlay" onclick="closeModal()"></div>
+
+
+<div class="Messagerie">
+<div class="Liste">
+    <?php 
+        $recupConversations = $bdd->prepare('SELECT id_des FROM conv WHERE id_au = ?');
+        $recupConversations->execute(array( $_SESSION['id']));
+        while($conversation = $recupConversations->fetch()){
+            $recupUser = $bdd->prepare('SELECT * FROM users WHERE id = ?');
+            $recupUser->execute(array($conversation['id_des']));
+            $user = $recupUser->fetch();
+            if($user['id'] != $_SESSION['id']){
+    ?>
+        <button class="bouton" onclick="redirectMessagerie(<?= $user['id']; ?>)">
+            <div class="case">
+                <img src="Image\<?=$user['profil']?>" class="icon1">
+                <p class="ut"><?= $user['pseudo']; ?></p>
             </div>
+        </button>
+    <?php
+            }
+        }
+    ?>
+</div>
+
+    <div class="ChatConteneur">
+    <?php if($getid != $_SESSION['id']){?>
+        <div class="header">
+            <img src="image\<?=$photo?>" class="icon2">
+            <a href="profil.php?id=<?= $getid ?>"><?= $cible ?></a>
         </div>
         <div class="message-container" id="message">  
             <?php 
@@ -184,10 +184,9 @@ if(isset($_POST['signaler'])) {
                                     <button class="report-btn" data-message-id="<?= $message['message']; ?>">Signaler</button>
                                 </div>
                             </div>
-                            <img src="logo.png" class="avatar"> 
+                            <img src="Image\<?=$photo?>" class="icon3">
                             <p><?= $message['message']; ?></p>
                         <?php } else { ?>
-                            <img src="logo.png" class="avatar"> 
                             <p><?= $message['message']; ?></p>
                             <div class="options-container droite">
                                 <span class="options-icon" onclick="showOptions(this)">...</span>
@@ -205,12 +204,14 @@ if(isset($_POST['signaler'])) {
         </div>
 
         <div class="messages">
-            <?php if($getid != $_SESSION['id']){?>
             <input type="text" id="messageInput" autocomplete="off" placeholder="Tapez votre message..."> 
             <button id="envoyerMessage">Envoyer</button>
             <?php
             }
-            ?>
+            else{?>
+                <div><p>Choisis une conversation</p></div>
+            <?php
+            }?>
         </div>
     </div> 
 </div>
@@ -242,33 +243,6 @@ if(isset($_POST['signaler'])) {
     }
     }
 
-    function optionProfil() {
-    var nomElement = document.querySelector('.header p'); 
-    var optionProfil1 = document.querySelector('.option-profil'); 
-
-    if (nomElement && optionProfil1) {
-    
-        var rect = nomElement.getBoundingClientRect();
-        var headerRect = document.querySelector('.header').getBoundingClientRect();
-
-        
-        optionProfil1.style.display = 'block';
-        optionProfil1.style.position = 'fixed';
-        optionProfil1.style.top = (headerRect.top + rect.bottom) + 'px'; 
-        optionProfil1.style.left = rect.left + 'px'; 
-        optionProfil1.style.transform = 'translate(0, 0)';
-    }
-}
-
-    function SignalementProfil() {
-        var signalementProfDiv = document.getElementById('signalementprof');
-        signalementProfDiv.style.display = 'block';
-        signalementProfDiv.style.position = 'fixed';
-        signalementProfDiv.style.top = '50%';
-        signalementProfDiv.style.left = '50%';
-        signalementProfDiv.style.transform = 'translate(-50%, -50%)';
-    }
-
     function Signalement() {
     var signalementDiv = document.getElementById('signalement');
     signalementDiv.style.display = 'block';
@@ -277,6 +251,7 @@ if(isset($_POST['signaler'])) {
     signalementDiv.style.left = '50%';
     signalementDiv.style.transform = 'translate(-50%, -50%)';
     }
+
 
 window.onclick = function(event) {
     let options = document.querySelectorAll('.option');
@@ -329,7 +304,7 @@ function reportMessage(category) {
                 signaler: true,
                 message: messageReport,
                 motif: category,
-                type : 'message',
+                type: 'message',
             },
             success: function(response) {
                 console.log("Message signalé avec succès");
@@ -343,30 +318,7 @@ function reportMessage(category) {
     }
 }
 
-function reportProfil(category) {
-    if (profil) {
-        $.ajax({
-            type: "POST",
-            url: "messagerie.php?id=<?= $getid; ?>",
-            data: {
-                signaler: true,
-                profil: profil,
-                motif: category,
-                type : 'profil',
-            },
-            success: function(response) {
-                console.log("Profil signalé avec succès");
-                var signalementDiv = document.getElementById('signalementprof');
-                signalementDiv.style.display = 'none';
-            },
-            error: function(xhr, status, error) {
-                console.error("Erreur lors du signalement du Profil :", error);
-            }
-        });
-    }
-}
-
-    function deleteMessage(messageId) {
+function deleteMessage(messageId) {
         $.ajax({
             type: "POST",
             url: "messagerie.php?id=<?= $getid; ?>",
@@ -407,20 +359,10 @@ function reportProfil(category) {
         Signalement();
     });
 
-    $(document).on('click', '.reportprof-btn', function() {
-        messageReport = $(this).data('pseudo-id');
-        SignalementProfil();
-    });
-
     var type;
     $(document).on('click', '.signalement', function() {
         var messageId = $(this).data('button');
         reportMessage(type);
-    });
-
-    $(document).on('click', '.signalementprof', function() {
-        var messageId = $(this).data('button');
-        reportProfil(type);
     });
 
     $('#messageInput').keypress(function(event) {
@@ -439,6 +381,25 @@ function reportProfil(category) {
     });
 
 });
+
+        function Signalement() {
+            var signalementDiv = document.getElementById('signalement');
+            signalementDiv.style.display = 'block';
+            var overlay = document.querySelector('.overlay');
+            overlay.style.display = 'block';
+        }
+
+        // Fonction pour fermer le modal
+        function closeModal() {
+            var signalementDiv = document.getElementById('signalement');
+            signalementDiv.style.display = 'none';
+            var overlay = document.querySelector('.overlay');
+            overlay.style.display = 'none';
+        }
+
+        $('.signalement').click(function() {
+            closeModal();
+    });
 
 </script>
 </body>
