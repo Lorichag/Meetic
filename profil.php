64e523db1f6e77cfdeefcfb218b72b5c3ba81cee
Soupsouu <134335994+Soupsouu@users.noreply.github.com>
2024-06-07 12:13:19 +0200

    Add files via upload

--- profil.php
+++ profil.php
@@ -64,6 +64,31 @@ $query->execute(array($_SESSION['id']));
 $userData = $query->fetch();
 $photo = $userData['profil']; 
 $abo = $userData['abo'];
+
+
+$recupUser = $bdd->prepare('SELECT * FROM users WHERE id NOT IN 
+    (SELECT id_sig FROM bloquer WHERE id_au = ?) 
+    AND id NOT IN (SELECT id_au FROM bloquer WHERE id_sig = ?) 
+    AND id != ? ORDER BY id DESC');
+$recupUser->execute(array($_SESSION['id'], $_SESSION['id'], $_SESSION['id']));
+
+
+if (isset($_GET['q']) && !empty($_GET['q'])) {
+    $q = htmlspecialchars($_GET['q']);
+    
+    $recupUser = $bdd->prepare('SELECT id, pseudo, profil FROM users 
+                                WHERE id NOT IN (SELECT id_sig FROM bloquer WHERE id_au = ?) 
+                                AND id NOT IN (SELECT id_au FROM bloquer WHERE id_sig = ?) 
+                                AND pseudo LIKE ? 
+                                ORDER BY id DESC');
+    $recupUser->execute(array($_SESSION['id'], $_SESSION['id'], "%$q%"));
+} else {
+    $recupUser = $bdd->prepare('SELECT id, pseudo, profil FROM users 
+                                WHERE id NOT IN (SELECT id_sig FROM bloquer WHERE id_au = ?) 
+                                AND id NOT IN (SELECT id_au FROM bloquer WHERE id_sig = ?) 
+                                ORDER BY id DESC');
+    $recupUser->execute(array($_SESSION['id'], $_SESSION['id']));
+}
 ?>
 
 <!DOCTYPE html>
@@ -84,9 +109,10 @@ $abo = $userData['abo'];
     </div>
     <ul class="nav-list">
       <li>
-        
           <i class='bx bx-search' ></i>
-         <input type="text" placeholder="Search...">
+            <form method="GET" action="connecting.php?id=<?= $_SESSION['id']; ?>">
+                <input type="search" name="q" placeholder="Recherche..." />
+            </form>
       </li>
       <li>
         <a href="connecting.php?id=<?= $_SESSION['id']; ?>">
@@ -184,7 +210,7 @@ $abo = $userData['abo'];
     </div>
 
     <div class="pub">
-      <img src="https://nuostore.com/wp-content/uploads/2023/11/IBANEZ-AE295WK-300x775.jpg">
+        <img id="randomImage" class="t" src="" alt="Publicité">
     </div>
   </div>
 
@@ -344,26 +370,25 @@ $abo = $userData['abo'];
 </html>
 
 <script>
-    function closeModal() {
-            var signalementDiv = document.getElementById('signalement');
-            signalementDiv.style.display = 'none';
-            var overlay = document.querySelector('.overlay');
-            overlay.style.display = 'none';
-    }
-
+  
     let sidebar = document.querySelector(".sidebar");
-    let decaler = document.querySelector(".decaler");
+    let decaler = document.querySelector((".decaler"))
     let closeBtn = document.querySelector("#btn");
     let searchBtn = document.querySelector(".bx-search");
-
-    closeBtn.addEventListener("click", () => {
+    closeBtn.addEventListener("click", ()=>{
+        sidebar.classList.toggle("open");
+        decaler.classList.toggle("open");
+    });
+    searchBtn.addEventListener("click", ()=>{
         sidebar.classList.toggle("open");
         decaler.classList.toggle("open");
     });
 
-    searchBtn.addEventListener("click", () => {
-        sidebar.classList.toggle("open");
-        decaler.classList.toggle("open");
+    function redirectMessagerie(userId) {
+        window.location.href = "profil.php?id=" + userId;
+    }
+    document.getElementById('log_out').addEventListener('click', function() {
+        document.getElementById('logout-form').submit();
     });
 
     $(document).ready(function() {
@@ -461,3 +486,13 @@ $abo = $userData['abo'];
         });
     }
 </script>
+<script>
+        const images = ["image/Pub.png", "image/Pub1.png", "image/Pub2.png", "image/Pub3.png", "image/Pub4.png"];
+
+        function getRandomImage() {
+            const randomIndex = Math.floor(Math.random() * images.length);
+            return images[randomIndex];
+        }
+        // Sélectionner l'élément img et définir son attribut src
+        document.getElementById('randomImage').src = getRandomImage();
+    </script>
